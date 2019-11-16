4cc283d1f69ef4560f1330a5373142c80b7f7090
untakenyjp <1980344834@qq.com>
2019-11-16 20:04:43 +0800

    !

---
+++ Mycalculator/src/github/chenjiangchao/Listener/ButtonResponse.java
@@ -0,0 +1,18 @@
+package github.chenjiangchao.Listener;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+
+public class ButtonResponse extends MouseAndKeyboardListener {
+    private JTextField resultText;
+
+    public ButtonResponse(JTextField JText)
+    {
+        resultText = JText;
+    }
+
+    public void mouseClicked(MouseEvent e)
+    {
+
+    }
+}
